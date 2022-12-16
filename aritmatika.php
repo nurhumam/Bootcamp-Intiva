8d6b810fe95682b634356c790763dd47fc52711f
nurhumam <120307728+nurhumam@users.noreply.github.com>
2022-12-16 21:52:05 +0700

    Add files via upload

---
+++ aritmatika.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "tipe data adalah bahasa dalam program untuk menentukan value dari suatu operasi <br>" ;
+	echo '<br>';
+echo "contoh tipe data: <br>" ;
+	echo "1. int= merupakan bilangan bulat, (contoh: 1 2 3) <br>" ;
+	echo "2. floating= bilangan pecahan atau desimal, (contoh: 1,2) <br>" ;
+	echo "3. char= menyimpan karakter alfabet, (contoh:char A,B A=2,B=3) <br>" ;
+	echo "4. boolean= mencari value true or false, (contoh:a=4, b=3, a>b) <br>" ;
+	echo "5. array= menyimpan sebuah element, (contoh:basket, futsal, panah, memiliki 3 array) <br>" ;
+	echo "6. string= mencakup teks dan diberi kutip, (contoh:'anda sangat rajin') <br>" ;
+
+	echo '<br>';
+echo "aritmatika dalam pemrograman adalah simbol yang menandakan sutau operasi, baik itu penjumlahan atau pengurangan dll <br>" ;
+	$a = 5;
+	$b = 5;
+
+$jawab1=$a + $b;
+$jawab2=$a - $b;
+$jawab3=$a / $b;
+$jawab4=$a % $b;
+
+
+	echo '<br>';
+echo "a= 5 dan b= 5 <br>";
+echo " penjumlahan : $jawab1 <br>" ;
+echo " pengurangan : $jawab2 <br>" ;
+echo " pembagian : $jawab3 <br>" ;
+echo " modulus : $jawab4 <br>" ;
+?>
