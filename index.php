d007b1a343dc2e9f416dd5e4ebd427b4b96d39d2
nurhumam <120307728+nurhumam@users.noreply.github.com>
2022-12-23 22:01:13 +0700

    upload index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$a=45;
+
+if ($a<45){
+       echo "Nilai : D </br>";
+       echo "Keterangan : Tidak Lulus";
+}else if ($a==45){
+       echo "Nilai : C </br>";
+       echo "Keterangan : Cukup";
+}else if ($a<=70){
+       echo "Nilai : B </br>";
+       echo "Keterangan : Bagus";
+}else if ($a>70){
+       echo "Nilai : A </br>";
+       echo "Keterangan : Mantab";
+}
+?>
